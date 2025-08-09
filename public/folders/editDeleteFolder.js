8f2f3e479745a568ea8e391feca7940d54adf10f
Julian So <ch.julian.so@gmail.com>
2025-08-08 22:27:48 -0400

    working on delete folder button

--- public/folders/editDeleteFolder.js
+++ public/folders/editDeleteFolder.js
@@ -1,7 +1,14 @@
 const deleteFolderModal = document.querySelector('.deleteFolderModal');
-const deleteFolderModalOpenBtn = document.querySelector('.deleteFolderModalOpenBtn');
-deleteFolderModalOpenBtn?.addEventListener('click', () => {
-    console.log('delete folder modal');
-    deleteFolderModal?.showModal();
+const deleteFolderModalOpenBtns = document.querySelectorAll('.deleteFolderModalOpenBtn');
+deleteFolderModalOpenBtns?.forEach((button) => {
+    const deleteMessage = document.querySelector('#deleteMessage');
+    button.addEventListener('click', () => {
+        const folderId = button.dataset.id;
+        const folderName = button.dataset.name;
+        if (deleteMessage) {
+            deleteMessage.textContent = `Are you sure you want to delete ${folderName}? You cannot undo this action.`;
+        }
+        deleteFolderModal?.showModal();
+    });
 });
 export {};
--- src/folders/editDeleteFolder.ts
+++ src/folders/editDeleteFolder.ts
@@ -1,9 +1,23 @@
 const deleteFolderModal: HTMLDialogElement | null = document.querySelector('.deleteFolderModal');
-const deleteFolderModalOpenBtn: HTMLDialogElement | null = document.querySelector(
+const deleteFolderModalOpenBtns: NodeListOf<HTMLButtonElement> | null = document.querySelectorAll(
   '.deleteFolderModalOpenBtn'
 );
+const form: HTMLFormElement | null = document.querySelector('#deleteFolderForm');
 
-deleteFolderModalOpenBtn?.addEventListener('click', () => {
-  console.log('delete folder modal');
-  deleteFolderModal?.showModal();
+deleteFolderModalOpenBtns?.forEach((button: HTMLButtonElement) => {
+  const deleteMessage: HTMLElement | null = document.querySelector('#deleteMessage');
+  button.addEventListener('click', () => {
+    const folderId = button.dataset.id;
+    const folderName = button.dataset.name;
+    if (form) {
+      form.action = `/folder/${folderId}?_method=DELETE`;
+      form.folderId.value = folderId;
+    }
+
+    if (deleteMessage) {
+      deleteMessage.textContent = `Are you sure you want to delete ${folderName}? You cannot undo this action.`;
+    }
+
+    deleteFolderModal?.showModal();
+  });
 });
