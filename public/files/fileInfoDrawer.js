93fbc17b87a8a7d795bd40f09ca81d32b44a66c3
Julian So <ch.julian.so@gmail.com>
2025-08-19 23:21:03 -0400

    add drawers to each button

--- public/files/fileInfoDrawer.js
+++ public/files/fileInfoDrawer.js
@@ -1,15 +1,13 @@
 // https://github.com/columk1/file-uploader/blob/9d61314fd1643b03f677871f6e5da58d4be99128/src/javascript/publicFolder.js#L28
 const openFileDrawer = document.querySelector('#open-file-drawer');
-const openDrawerBtns = document.querySelectorAll('.open-drawer');
-const drawer = document.querySelector('#drawer-overview');
-const openButton = drawer?.nextElementSibling;
-openButton?.addEventListener('click', () => {
-    if (drawer) {
+const drawer = document.querySelector('.drawer-overview');
+const openDrawerBtns = document.querySelectorAll('.open-drawer-btn');
+openDrawerBtns.forEach((openButton) => {
+    openButton.addEventListener('click', () => {
         drawer.open = true;
-    }
+    });
 });
 openDrawerBtns.forEach((btn) => {
-    console.log('open drawer');
     return;
     // btn.addEventListener('click', () => {
     //   if (openFileDrawer) {
@@ -18,4 +16,3 @@ openDrawerBtns.forEach((btn) => {
     // });
 });
 export {};
-// name, type, size, created, modified
--- src/files/fileInfoDrawer.ts
+++ src/files/fileInfoDrawer.ts
@@ -1,22 +1,16 @@
 // https://github.com/columk1/file-uploader/blob/9d61314fd1643b03f677871f6e5da58d4be99128/src/javascript/publicFolder.js#L28
 
 const openFileDrawer: HTMLElement | null = document.querySelector('#open-file-drawer');
-const openDrawerBtns: NodeListOf<HTMLButtonElement> | null =
-  document.querySelectorAll('.open-drawer');
+const drawer = document.querySelector<HTMLElement | (null & { open: boolean })>('.drawer-overview');
+const openDrawerBtns = document.querySelectorAll<HTMLElement>('.open-drawer-btn');
 
-const drawer = document.querySelector('#drawer-overview') as
-  | (HTMLElement & { open: boolean })
-  | null;
-const openButton = drawer?.nextElementSibling;
-
-openButton?.addEventListener('click', () => {
-  if (drawer) {
-    drawer.open = true;
-  }
+openDrawerBtns.forEach((openButton) => {
+  openButton.addEventListener('click', () => {
+    (drawer as any).open = true;
+  });
 });
 
 openDrawerBtns.forEach((btn) => {
-  console.log('open drawer');
   return;
   // btn.addEventListener('click', () => {
   //   if (openFileDrawer) {
@@ -24,4 +18,3 @@ openDrawerBtns.forEach((btn) => {
   //   }
   // });
 });
-// name, type, size, created, modified
